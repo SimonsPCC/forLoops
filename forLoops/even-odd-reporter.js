7cf95cce0428e3ad596d4377ec3fbb249fcf4c3c
SimonsPCC <73341402+SimonsPCC@users.noreply.github.com>
2021-01-26 23:18:39 +0100

    Add files via upload

---
+++ forLoops/even-odd-reporter.js
@@ -0,0 +1,12 @@
+for (var x = 1; x <= 20; x++) {
+        if (x % 2 === 0) {
+                console.log(x + " is even");
+        }
+
+        else {
+                console.log(x + " is oneven");
+        }
+
+}
+
+console.log("Alles boven te 20 is te moeilijk")
